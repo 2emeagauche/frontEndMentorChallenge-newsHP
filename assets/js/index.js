9db242e617b56033093da0bc041c48a5085d00dc
PY Chassaigne <pychassaigne@gmail.com>
2023-05-04 20:41:36 +0200

    second scss amend plus adding js specific class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
-const openMenuButton = window.document.querySelector('.open-menu');
-const closeMenuButton = window.document.querySelector('.close-menu');
-const nav = window.document.querySelector('#main-nav');
+const openMenuButton = window.document.querySelector('.js-open-menu');
+const closeMenuButton = window.document.querySelector('.js-close-menu');
+const nav = window.document.querySelector('.main-nav');
 
 openMenuButton.addEventListener('click', (e) => {
     nav.classList.add('menu-open');
